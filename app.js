21ad9b080770cb1edfe56ae0e7c0b823deb247b8
Md Mostafa <mdmostafahakim@gmail.com>
2024-06-27 23:53:02 +0600

    my commit

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'https://your-allowed-origin.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.get('/', (req, res) => {
     res.send("Welcome")
