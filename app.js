fefac1586eacc2b4b869bc9a6bad6deae47f06f2
Md Mostafa <mdmostafahakim@gmail.com>
2024-06-27 23:17:57 +0600

    my commit

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors')
 const teacherRouter = require('./routes/teacherRoute')
+const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
 const db = mongoose.connection;
-db.on('error', (err) => {
-    console.log("db is not connected:", err)
-})
-
-
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -25,5 +25,9 @@ app.use('/', (req, res) => {
 app.use('/api/teacher', teacherRouter)
 
 
+
+app.listen(PORT, () => {
+    console.log(`server is start at the port of ${PORT}`)
+})
 
 module.exports = app;
