d139807cb4b510ff4347985661cbc6a95bdf52ed
Md Mostafa <mdmostafahakim@gmail.com>
2024-06-28 00:05:53 +0600

    clint

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react"
+
+function App() {
+  useEffect(() => {
+
+  })
+  return (
+    <>
+      <div className="bg-green-600 text-white">
+        <h2>Name</h2>
+        <span>age</span>
+      </div>
+    </>
+  )
+}
+
+export default App
